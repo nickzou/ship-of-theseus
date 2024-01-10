58ee5901d1489007b49d9bc6df46bf9ac394d012
Nick Zou <z.nick00@gmail.com>
2024-01-09 22:59:06 -0500

    add some styling

--- wp/themes/ship-of-theseus/index.php
+++ wp/themes/ship-of-theseus/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello World</title>
+    <script src="https://cdn.tailwindcss.com"></script>
     <?php wp_head(); ?>
 </head>
 <body>
@@ -17,12 +18,22 @@ $args = array(
 
 $tasks = new WP_Query( $args );
 
-if ( $tasks->have_posts() ) : 
+if ( $tasks->have_posts() ) : ?>
+<ul>
+<?php
     while ( $tasks->have_posts() ) : $tasks->the_post(); 
         // Display the post's title and content
-        echo '<h1>' . get_the_title() . '</h1>';
-        the_content();
-    endwhile; 
+    ?>
+    <li>
+        <div class="p-3">
+            <h2 class="text-sm font-normal"><?php echo get_the_title(); ?></h2>
+            <div class="text-xs font-light"><?php echo get_the_content(); ?></div>
+        </div>
+    </li>
+    <?php
+    endwhile; ?>
+    </ul>
+    <?php
 else : 
     echo '<p>No posts found.</p>';
 endif; 
