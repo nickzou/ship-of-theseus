ec4e9ebc82845f638a0db8fd8f0b576a5a3969ea
Nick Zou <z.nick00@gmail.com>
2024-01-12 21:00:48 -0500

    add tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from 'tailwindcss';
+
+const config: Config = {
+	content: ["wp/themes/ship-of-theseus/**/*.php"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
+  
+export default config;
+  
